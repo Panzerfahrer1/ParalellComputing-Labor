22bc5174d3d8d2fcb6582ea483c4d5d3415c4dd2
Panzerfahrer1 <oliverangerer0@gmail.com>
2026-01-07 08:40:46 +0100

    DeadLock tutorial project added

---
+++ Dead Lock/Program.cs
@@ -0,0 +1,15 @@
+﻿// Deadlock Simulation
+object lockA = new object();
+object lockB = new object();
+
+Thread thread1 = new Thread(() =>
+{
+    lock (lockA)
+    {
+        Thread.Sleep(1000);
+        lock (lockB)
+        {
+            Console.WriteLine("Thread 1 acquired both locks.");
+        }
+    }
+});
