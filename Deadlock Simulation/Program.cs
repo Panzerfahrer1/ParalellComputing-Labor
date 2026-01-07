754d0a2c6256e10161366ea1c4ab7629aed8f07b
Panzerfahrer1 <oliverangerer0@gmail.com>
2026-01-07 10:14:24 +0100

    Deadlock Aufgabe

---
+++ Deadlock Simulation/Program.cs
@@ -0,0 +1,60 @@
+﻿List<int> from = new(10000);
+List<int> to = new(10000);
+
+for(int i = 0; i < 10000; i++)
+{
+    from.Add(i);
+}
+
+object lockA = new object();
+object lockB = new object();
+
+Thread thread1 = new(() => CopyReverse(from, to));
+Thread thread2 = new(() => Copy(from, to));
+
+thread1.Start();
+thread2.Start();
+thread1.Join();
+thread2.Join();
+
+void CopyReverse(List<int> from, List<int> to)
+{
+    lock (lockA)
+    {
+        lock (lockB)
+        {
+            to.Clear();
+            for(int i = 0; i < from.Count; i++)
+            {
+                to.Add(from[from.Count - 1 - i]);
+            }
+
+            Console.WriteLine("Fertig");
+
+            Thread.Sleep(1000);
+        }
+    }
+
+    CopyReverse(from, to);
+}
+
+void Copy(List<int> from, List<int> to)
+{
+    lock (lockA)
+    {
+        lock (lockB)
+        {
+            to.Clear();
+            for (int i = 0; i < from.Count; i++)
+            {
+                to.Add(from[i]);
+            }
+
+            Console.WriteLine("Fertig");
+
+            Thread.Sleep(1000);
+        }
+    }
+
+    CopyReverse(from, to);
+}
