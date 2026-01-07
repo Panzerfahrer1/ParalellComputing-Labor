a00497ee4b7719960d1ca2b4c7474a71e82b2bd8
Panzerfahrer1 <oliverangerer0@gmail.com>
2026-01-07 08:49:51 +0100

    Deadlock Tutorial finished

--- Dead Lock/Program.cs
+++ Dead Lock/Program.cs
@@ -4,12 +4,35 @@ object lockB = new object();
 
 Thread thread1 = new Thread(() =>
 {
+    //Thread 1 Locks A
     lock (lockA)
     {
         Thread.Sleep(1000);
+        // While T1 does some work, Thread 2 starts.
+        // Since T2 locks B, T1 Cant continute
+        // And since T1 looks A, T2 cant continute => deadlock
+        // To solve this we always should have the same order of locks (A, B, C ,....)
         lock (lockB)
         {
-            Console.WriteLine("Thread 1 acquired both locks.");
+            Console.WriteLine("Thread 1 finished ...");
         }
     }
 });
+
+Thread thread2 = new Thread(() =>
+{
+    lock (lockB)
+    {
+        Thread.Sleep(1000);
+        lock (lockA)
+        {
+            Console.WriteLine("Thread 2 finished ...");
+        }
+    }
+});
+
+thread1.Start();
+thread2.Start();
+
+thread2.Join();
+thread1.Join();
