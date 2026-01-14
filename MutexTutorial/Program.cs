9f079e7784cb5033ea06dd0e19ff9cc30b887780
Panzerfahrer1 <oliverangerer0@gmail.com>
2026-01-14 09:00:32 +0100

    Mutex tutorial

--- MutexTutorial/Program.cs
+++ MutexTutorial/Program.cs
@@ -1,7 +1,8 @@
 ﻿int _count = 0;
-Mutex mutex = new Mutex(); // lokal, prozessintern (wenn ich keinen Namen mitgebe) => also wie Lock
+Mutex lockMutex = new Mutex(); // lokal, prozessintern (wenn ich keinen Namen mitgebe) => also wie Lock
 
-mutex.WaitOne(); // betritt kritischen Abschnitt
+#region MutexAlsLock
+lockMutex.WaitOne(); // betritt kritischen Abschnitt
 // Immer mit try, dort passiert der Zugriff
 try
 {
@@ -9,5 +10,40 @@ try
 }
 finally
 {
-    mutex.ReleaseMutex(); // Wieder Freigeben
+    lockMutex.ReleaseMutex(); // Wieder Freigeben
 }
+
+#endregion
+
+#region propper Mutex
+// Wenn true, setzt er den Mutex SOFORT
+// Wenn name, wird der Mutex Gesetzt => Es bedeutet es ist nur für meinen User gelocked
+// Wenn man "Global\\" Schreibt gilt der Lock für jeden User.
+// Mit out Created new kann man sicherstellen dass es nur einmal läuft. 
+//  Wenn es einen neuen Mutex erstellt ist es true, wenn ein anderer Prozess auch einen erstellen will, ist es false
+bool createdNew = false;
+using (Mutex mutex = new Mutex(false, "Global\\MyMutex", out createdNew))
+{
+    Console.WriteLine("Warte auf einen mutex...");
+    if (mutex.WaitOne(TimeSpan.FromSeconds(5.0))) //Hier wird der Mutex gesetzt und wenn er gesetzt wird => True; wenn schon gesperrt => false?
+    //Wenn er innerhalb der 5s den Mutex nicht bekommt wird es false => Er springt ins else
+    {
+        try
+        {
+            // Kritischer Bereich
+            Console.WriteLine("Wrote new file...");
+            Thread.Sleep(10000);
+        }
+        finally
+        {
+            mutex.ReleaseMutex();
+        }
+    }
+    else
+    {
+        Console.WriteLine("Konnte Mutex nicht erlangen...");
+        Thread.Sleep(3000);
+    }
+}
+
+#endregion
