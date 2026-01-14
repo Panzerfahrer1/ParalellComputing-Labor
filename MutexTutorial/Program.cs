aa3e1b47c8b3e8941a1705df257748026c3a4b1b
Panzerfahrer1 <oliverangerer0@gmail.com>
2026-01-14 08:35:31 +0100

    Mutex Tutorial V1

---
+++ MutexTutorial/Program.cs
@@ -0,0 +1,13 @@
+﻿int _count = 0;
+Mutex mutex = new Mutex(); // lokal, prozessintern (wenn ich keinen Namen mitgebe) => also wie Lock
+
+mutex.WaitOne(); // betritt kritischen Abschnitt
+// Immer mit try, dort passiert der Zugriff
+try
+{
+    _count++;
+}
+finally
+{
+    mutex.ReleaseMutex(); // Wieder Freigeben
+}
