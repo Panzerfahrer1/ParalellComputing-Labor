70937214d25a687ffe0ffb4aa19280f1c34e635b
Panzerfahrer1 <oliverangerer0@gmail.com>
2026-01-14 09:17:42 +0100

    Semaphore Tutorial

---
+++ Semaphore/Program.cs
@@ -0,0 +1,17 @@
+﻿SemaphoreSlim _sem = new SemaphoreSlim(3); // Capacity of 3
+
+for(int i = 0; i < 5; i++)
+{
+    new Thread(Enter).Start(i);
+}
+
+void Enter(object id)
+{
+    Console.WriteLine(id + " wants to enter");
+    _sem.Wait();
+    //critical area
+    Console.WriteLine(id + " is in!");
+    Thread.Sleep(1000 + (int)id);
+    Console.WriteLine(id + " is leaving...");
+    _sem.Release();
+}
